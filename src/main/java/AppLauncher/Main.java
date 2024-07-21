fe341c90ede24b5b0ca584231482d2fef5243b1a
zkhr <zakhar864@gmail.com>
2024-07-21 17:50:59 +0200

    Created not completed  logic of sending notifications

--- src/main/java/AppLauncher/Main.java
+++ src/main/java/AppLauncher/Main.java
@@ -1,13 +1,27 @@
 package AppLauncher;
 
 
+import Parce.MonoParcer;
+import Parce.NBUParcer;
+import Parce.PrivatParcer;
 import TelegramBot.TelegramCurrencyBot;
+import dto.MonoDto;
+import dto.NbuDto;
+import dto.PrivatDto;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+
+        PrivatParcer privatParcer = new PrivatParcer();
+        List<PrivatDto> request = privatParcer.getRequest();
+        request.stream().forEach(System.out::println);
+
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new TelegramCurrencyBot());
